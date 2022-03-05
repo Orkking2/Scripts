722900bbe2506f885a6244c5d8ccbe8eb6b4a120
Orkking2 <nicolas.bird.voneyben@gmail.com>
2022-03-04 22:20:19 -0800

    update

--- Games/Battle_Bots.py
+++ Games/Battle_Bots.py
@@ -5,7 +5,7 @@ class player:
        self.health = health + 10*handicap
        self.attack = attack + handicap
     def ai (self, level):
-        ai_bias(level)
+        pass
 
 def handicap_questions ():
     handicap_switch = input("Would you like a handicap? ")
@@ -17,13 +17,29 @@ def handicap_questions ():
         handicap = 0
         return(handicap)
 
-handicap = handicap_questions()
-you = player(handicap, 100, 5)
+def init ():
+    handicap = handicap_questions()
+    human = player(handicap, 100, 5)
+    ai_level = int(input("What level would you like your opponent to be? (1-5) ")) % 6
+    bot = player(ai_level,100,5)
+    return(handicap,human,ai_level,bot)
 
 
-def ai_bias (level):
-    pass
+def ai_bias (level,opponent_hp,opponent_attack):
+    if level == 0:
+        return()
+    elif level == 1:
+        pass
+    elif level == 2:
+        pass
+    elif level == 3:
+        pass
+    elif level == 4:
+        pass
+    elif level == 5:
+        pass
 
 def game ():
-    while life == True:
-        pass
+    handicap, human, ai_level, bot = init()
+    while life:
+        return()
