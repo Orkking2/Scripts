99a1ceb1353acdc8b50ca81ad81b9d331132113c
Orkking2 <nicolas.bird.voneyben@gmail.com>
2022-03-09 09:46:46 -0800

    x -> i

--- Euler/Matrix.py
+++ Euler/Matrix.py
@@ -29,9 +29,9 @@ def Matrix_Generator (XMax,XMin,YMax,YMin,D):
     XList = []
     YList = []
 
-    for x in range(1,D[2]+1):
-        XList.append(MatrixX[(x - 1) % D[0]])
-        YList.append(MatrixY[floor((x - 1)/D[0])])
+    for i in range(1,D[2]+1):
+        XList.append(MatrixX[(i - 1) % D[0]])
+        YList.append(MatrixY[floor((i - 1)/D[0])])
 
     MatrixXY = Matrix(XList,YList)
 
