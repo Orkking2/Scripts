6975e876ed53986556fc96329dd3a27d5a52f471
Orkking2 <nicolas.bird.voneyben@gmail.com>
2022-03-08 17:07:16 -0800

    update

--- Euler/Display.py
+++ Euler/Display.py
@@ -1,3 +1,20 @@
-from Approximater import *
+# from Approximater import *
+from Matrix import *
+import matplotlib.pyplot as plt
+
+YMax = 1
+YMin = -1
+XMax = 1
+XMin = -1
+
+D = [2,3] # x, y
+
+MatrixXY = Matrix_Generator(XMin,XMax,YMax,YMin,D)
+
+print(MatrixXY.x)
+
+r = min((XMax-XMin)/(2*(D[0]-1)),(YMax-YMin)/(2*(D[1]-1)))*0.9
+
+
 
 ## Soon TM
--- Euler/Matrix.py
+++ Euler/Matrix.py
@@ -1,34 +1,44 @@
-# import numpy as np
-
 def floor (num):
     if round(num) <= num:
         return(round(num))
     else:
         return(round(num)-1)
 
+class Matrix:
+    def __init__(self,XComponent,YComponent):
+        self.x = XComponent
+        self.y = YComponent
+
 YMax = 1
 YMin = -1
 XMax = 1
 XMin = -1
 
-D = [6,7] # x, y
+D = [2,3] # x, y
 
-XDist = XMax - XMin
-YDist = YMax - YMin
+def Matrix_Generator (XMax,XMin,YMax,YMin,D):
+    XDist = XMax - XMin
+    YDist = YMax - YMin
 
-XSpacing = XDist/(D[0]-1)
-YSpacing = YDist/(D[1]-1)
+    XSpacing = XDist/(D[0]-1)
+    YSpacing = YDist/(D[1]-1)
 
-MatrixX = []
-MatrixY = []
+    MatrixX = []
+    MatrixY = []
 
-for i in range(D[0]):
-    MatrixX.append(i*XSpacing)
-for i in range(D[1]):
-    MatrixY.append(i*YSpacing)
+    for i in range(D[0]):
+        MatrixX.append(i*XSpacing + XMin)
+    for i in range(D[1]):
+        MatrixY.append(i*YSpacing + YMin)
 
-Md = [int(x) for x in str(D[0]*D[1])]
+    XList = []
+    YList = []
 
-MatrixXY = [(MatrixX[((x - 1) % D[0]) + 1], MatrixY[floor((x - 1)/D[0])]) for x in Md]
+    for x in range(1,D[0]*D[1]+1):
+        XList.append(MatrixX[(x - 1) % D[0]])
+        YList.append(MatrixY[floor((x - 1)/D[0])])
 
-print(MatrixXY)
+    MatrixXY = Matrix(XList,YList)
+
+    return(MatrixXY)
+
