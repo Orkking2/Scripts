3fe370ce07c3ec51083f0599cce72847f4135730
Orkking2 <nicolas.bird.voneyben@gmail.com>
2022-03-10 23:31:18 -0800

    update

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -1,66 +1,78 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <math.h>
-using namespace std;
 
-class Matrix{
-    public:
-        Matrix(point PMin, point PMax, int Dim[2]){
-            XDist = PMax.x - PMin.y;
-            YDist = PMax.y - PMin.y;
-        
-            D[3] = {Dim[0], Dim[1], Dim[0]*Dim[1]};
-
-            XSpacing = XDist/(D[0]-1);
-            YSpacing = YDist/(D[1]-1);
-
-            for(int i = 0; i < D[0]; i++){
-                MatrixX[i] = (i*XSpacing + PMin.x);
-            }
-            for(int i = 0; i < D[1]; i++){
-                MatrixY[i] = (i*YSpacing + PMin.y);
-            }
-            for(int i = 0; i < D[2]; i++){
-                XList[i] = MatrixX[i % D[0]];
-                YList[i] = MatrixY[floor(i/D[0])];
-            }
-            
-            double Matrix[D[2]][2];
-            
-            for(int i = 0; i < D[2]; i++){
-                for(int j = 0; j < 2; j++){
-                    if(j == 0){
-                        Matrix[i][j] = XList[i];
-                    } else {
-                        Matrix[i][j] = YList[i];
-                    }
-                }
-            }
-        };
-    private:
-        int D[3];
-        double MatrixX[D[0]];
-        double MatrixY[D[1]];
-        double XDist;
-        double YDist;
-        double XSpacing;
-        double YSpacing;
-        double XList[D[2]];
-        double YList[D[2]];
-};
-
-class point{
-    point(double XComponent, double YComponent){
+struct Point{
+    Point(double XComponent, double YComponent){
         x = XComponent;
         y = YComponent;
     }
+    double x;
+    double y;
+};
+
+class Matrix{
     public:
-        double x;
-        double y;
+        Matrix(Point pMin, Point pMax, std::vector<int> dimensions){
+            xDist = pMax.x - pMin.y;
+            yDist = pMax.y - pMin.y;
+    
+            int d[3] = {dimensions[0], dimensions[1], dimensions[0]*dimensions[1]};
+
+            xSpacing = xDist/(d[0]-1);
+            ySpacing = yDist/(d[1]-1);
+
+            
+
+
+            for(int i = 0; i < d[0]; i++){
+                matrixX.push_back(i*xSpacing + pMin.x); 
+            }
+            for(int i = 0; i < d[1]; i++){
+                matrixY.push_back(i*ySpacing + pMin.y);
+            }
+            for(int i = 0; i < d[2]; i++){
+                xList.push_back(matrixX[i % d[0]]);
+                yList.push_back(matrixY[floor(i/d[0])]);
+            }
+            
+            data.reserve(d[2]);
+            
+            for(int i = 0; i < d[2]; i++){
+                data[i].reserve(2);
+                for(int j = 0; j < 2; j++){
+                    if(j == 0){
+                        data[i][j] = xList[i];
+                    } else {
+                        data[i][j] = yList[i];
+                    }
+                }
+            }
+        }
+        const std::vector <std::vector<double>>& getMatrix(){
+            return data;
+        }
+    private:
+        std::vector<double> matrixX;
+        std::vector<double> matrixY;
+        double xDist;
+        double yDist;
+        double xSpacing;
+        double ySpacing;
+        std::vector<double> xList;
+        std::vector<double> yList;
+        std::vector <std::vector<double>> data;
 };
 
 int main()
 {
-    cout << "Test"; 
+    std::cout << "test";
+    Matrix m(Point (-10,-10), Point (10,10), {5,5});
+    auto& d = m.getMatrix();
+    for(auto& out : d){
+        std::cout << out[0] << std::endl;
+        std::cout << out[1] << std::endl;
+    }
     return 0;   
 }
